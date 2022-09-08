4614d710e4ef674b01061bb270c8a568bbfbaa09
Eifro <afrotricks19@gmail.com>
2022-09-08 12:14:05 -0500

    about end desktop

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,17 @@
 import NavBar from "./NavBar";
+import { Link } from 'react-router-dom';
+
+/* imp font awesome */
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowTurnDown, faMugHot } from "@fortawesome/free-solid-svg-icons";
 
 export default function About() {
   return (
     <div className="about text-white">
       <NavBar />
-      <div className="container mx-auto border text-center">
-        <h1 className="font-bold text-4xl tracking-widest my-5">CLIMATOONS</h1>
-        <div className="description border w-4/5 mx-auto flex flex-col gap-2 text-lg">
+      <div className="container mx-auto text-center mt-12">
+        <h1 className="font-bold text-4xl tracking-widest my-7">CLIMATOONS</h1>
+        <div className="description w-4/5 mx-auto flex flex-col gap-2 text-lg mb-12">
           <p>
             Aplicación Web que permite insertar un país o ciudad y ver su estado
             de clima actual en dicha ubicación.
@@ -25,8 +30,9 @@ export default function About() {
             <span className="text-yellow-500 text-2xl">♥</span> durante mi
             cursada en el bootcamp <span className="text-yellow-500 font-bold text-xl">Henry</span>.
           </p>
-          <p className="font-bold tracking-wider text-xl text-shadow-xl">Made with Miguel Espiritu</p>
+          <p className="font-bold tracking-wider text-2xl text-shadow-xl mt-10">Made with Miguel Espiritu <FontAwesomeIcon icon={faMugHot} className="text-yellow-500" /></p>
         </div>
+        <Link to='/' className="btn-back-home border-2 px-3 py-2 rounded text-lg font-bold uppercase tracking-wide hover:bg-white hover:text-zinc-600 transition duration-600">Back <FontAwesomeIcon icon={faArrowTurnDown} className="rotate-90" /> </Link>
       </div>
     </div>
   );
